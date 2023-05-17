1ace460baf1c671857e71b2ac0bc3fe211e420ac
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-05-17 18:30:49 +0300

    Add test cases

---
+++ Tests/handle_test.test.js
@@ -0,0 +1,17 @@
+const { expect }  = require('jest-mock');
+import store from '../src/modules/Store.js';
+const obj = {
+    description: 'hi',
+    completed: false,
+    index: 1,
+  };
+describe('AddItem',()=>{
+    test('testAdd',()=>{
+               expect(store.add(obj)).toBe('success');
+    });
+
+
+}
+
+
+);
